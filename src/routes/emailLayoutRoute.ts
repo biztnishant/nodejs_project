332e45129a3034cbf49416a56af2a7b94645c7ea
biztnishant <nishant.dhiman@biztecno.net>
2025-01-28 15:31:56 +0530

    updated route

--- src/routes/emailLayoutRoute.ts
+++ src/routes/emailLayoutRoute.ts
@@ -2,5 +2,5 @@ const express=require('express');
 const { createEmailLayout, getEmailLayouts }=require('../controllers/emailLayout');
 const router = express.Router();
 router.post("/createEmailLayout", createEmailLayout);
-router.get("/getEmailLayouts", getEmailLayouts)
+router.get("/getEmailLayouts", getEmailLayouts);
 export default router;
